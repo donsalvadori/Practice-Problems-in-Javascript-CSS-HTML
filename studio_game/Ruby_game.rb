883f2d5007b1f6d4528078f50696fac4712e817b
Chris Salvi <csalvi42@gmail.com>
2014-10-22 21:23:56 -0700

    beginning of game

---
+++ studio_game/Ruby_game.rb
@@ -0,0 +1,16 @@
+player1 = 'Larry'
+health1 = 60
+
+puts "#{player1}'s health is #{health1}"
+puts "#{player1}'s health is #{health1 * 3}"
+
+puts "#{player1}'s health is #{health1 / 9.0}"
+puts "#{player1}'s health is #{health1 /9}"
+
+puts "Players: \n \t larry \n \t curly \n \t moe"
+
+name1 = 'larry'
+name2 = 'curly'
+name3 = 'moe'
+
+puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
